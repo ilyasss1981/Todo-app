0e610acc3982892578fcd789e0fd3cd028c83f57
ILYAS NASIROV <ilyasss1981@gmail.com>
2020-12-21 04:14:12 +0300

    Todo_3_step Server And Loader

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import TodoList from './Todo/TodoList'
 import Context  from './context'
 import AddTodo from './Todo/AddTodo'
+import Loader from './Loader'
 
 function App() {
 
-  const [todos, setTodos] = React.useState([
-    {id: 1, completed: false, title: 'Купить хлеб'},
-    {id: 2, completed: false, title: 'Купить масло'},
-    {id: 3, completed: false, title: 'Купить молоко'}
-  ])
+  const [todos, setTodos] = React.useState([])
+  const [loading, setLoading] = React.useState(true)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(() => {
+          setTodos(todos)
+          setLoading(false)
+        }, 2000)        
+      })
+  }, [])
 
   function toggleTodo(id) {
     setTodos(
@@ -45,7 +54,8 @@ function App() {
       <div className='wrapper'>
         <h1>React tutorial</h1>
         <AddTodo onCreate={addTodo} />
-        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo} /> : <p>No todos!</p>}
+        {loading && <Loader />}
+        {todos.length ? <TodoList todos={todos} onToggle={toggleTodo} /> : loading ? null : <p>No todos</p>}
         
       </div>
     </Context.Provider>
